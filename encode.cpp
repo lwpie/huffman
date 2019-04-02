615560cd859b0a8954274702c95ce9634152386c
刘一芃 <liuyp18@mails.tsinghua.edu.cn>
2019-04-03 00:18:13 +0800

    Accept command line parameters

--- encode.cpp
+++ encode.cpp
@@ -1,10 +1,36 @@
 #include "huffman.h"
 
-int main()
+int main(int argv, char *argc[])
 {
-	// read entire file to memory
+	// get command line parameters
+	if ((argv != 4) ||
+		((strcmp(argc[1], "-o") != 0) && (strcmp(argc[3], "-o") != 0)))
+	{
+		std::cerr << "Invalid Input" << std::endl;
+		return 1;
+	}
+	std::string infile, outfile;
+	if (strcmp(argc[1], "-o") == 0)
+	{
+		outfile = argc[2];
+		infile = argc[3];
+	}
+	else
+	{
+		outfile = argc[3];
+		infile = argc[2];
+	}
 	std::ifstream in;
-	in.open("input", std::ios::binary);
+	in.open(infile, std::ios::binary);
+	std::ofstream out;
+	out.open(outfile, std::ios::binary);
+	if (!in || !out)
+	{
+		std::cerr << "File Open Failure" << std::endl;
+		return 1;
+	}
+
+	// read entire file to memory
 	std::filebuf *ptr;
 	ptr = in.rdbuf();
 	long size = ptr->pubseekoff(0, std::ios::end, std::ios::in);
@@ -12,13 +38,13 @@ int main()
 	char *buffer = new char[size];
 	ptr->sgetn(buffer, size);
 	in.close();
+
 	// encode
 	HuffmanTable hash;
 	for (long i = 0; i < size; i++)
 		hash.insert(buffer[i]);
+
 	// write into file
-	std::ofstream out;
-	out.open("output", std::ios::binary);
 	hash.encode(buffer, size, out);
 	free(buffer);
 	return 0;
